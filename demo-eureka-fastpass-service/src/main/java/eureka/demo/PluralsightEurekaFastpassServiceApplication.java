24a233bb1fc03fb669271c1189011ac4922e7d0b
nandakumarpurohit <nandakumarpurohit@gmail.com>
2019-08-21 08:36:51 -0400

    new files

---
+++ demo-eureka-fastpass-service/src/main/java/eureka/demo/PluralsightEurekaFastpassServiceApplication.java
@@ -0,0 +1,12 @@
+package eureka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightEurekaFastpassServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightEurekaFastpassServiceApplication.class, args);
+	}
+}
